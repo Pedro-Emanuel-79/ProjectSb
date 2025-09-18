ee0f7c60cffa4bd05fb77e2d3fb726508c9110ae
Pedro <emanuelpedrossp@gmail.com>
2025-09-17 23:39:35 -0300

    Initial Commit
    Primeiro commit

---
+++ ProjectSb/src/main/java/com/example/ProjectSb/ProjectSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectSb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSbApplication.class, args);
+	}
+
+}
